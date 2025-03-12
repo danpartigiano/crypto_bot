ce0ab48a720293e1d082c8f99cfc8908cf2b5626
arnell <arnellj@vt.edu>
2025-03-12 15:49:18 -0400

    update trade history

--- frontend/src/components/TradeHistory.js
+++ frontend/src/components/TradeHistory.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
 function TradeHistory() {
+    const mockTrades = [
+        { id: 12, cryptoPair: 'USD', amount: 1.2, results: 'Failed', date: '2024-01-01'},
+        { id: 12, cryptoPair: 'USD', amount: 1.2, results: 'Failed', date: '2024-01-01'},
+        { id: 12, cryptoPair: 'USD', amount: 1.2, results: 'Failed', date: '2024-01-01'},
+    ];
+
+
     return (
         <div>
             <h2>Trade History</h2>
             <p>Find past trades here.</p>
+            <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '10px '}}>
+                <thead>
+                    <tr>
+                        <th>Crypto Pair</th>
+                        <th>Amount</th>
+                        <th>Results</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mockTrades.map((trade) => (
+                        <tr key={trade.id}>
+                            <td>{trade.cryptoPair}</td>
+                            <td>{trade.amount}</td>
+                            <td>{trade.results}</td>
+                            <td>{trade.date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
