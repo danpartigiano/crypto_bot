16010e02c28ebbeb8984d520075eae28e884671b
arnell <arnellj@vt.edu>
2025-03-12 15:37:16 -0400

    update trade execution page

--- frontend/src/components/TradeExecution.js
+++ frontend/src/components/TradeExecution.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TradeExecution() {
+    const [cryptoPair, setCryptoPair] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const tradeOutPut = (e) => {
+        e.preventDefault();
+        //Mock tade output
+        alert(`Trading for ${cryptoPair} with amount ${amount}`);
+    };
+
+
     return (
         <div>
             <h2>Trade Execution</h2>
-            <p>Trades are done here.</p>
+            <form onSubmit={tradeOutPut}>
+                <div>
+                    <label>Crypto Pair: </label>
+                    <input
+                        type="text"
+                        value={cryptoPair}
+                        onChange={ (e) => setCryptoPair(e.target.value)}
+                        placeholder="e.g. USD"
+                    />
+                </div>
+                <div>
+                    <label>Amount: </label>
+                    <input
+                        type="number"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        placeholder="Amount being traded"
+                    />
+                </div>
+                <button type="submit">Execute Trade</button>
+            </form>
         </div>
     );
 }
