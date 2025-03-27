2faa22e56aa495455f4a3faeea6f4891ba8e8a03
arnell <arnellj@vt.edu>
2025-03-26 21:22:29 -0400

    Update Homepage.js

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -7,10 +7,10 @@ function Homepage() {
   return (
     <div className="full-page">
       <h1>Crypto Bot</h1>
+      <button className="nav-button" onClick={() => navigate('/link-coinbase')}>Coinbase</button>
       <button className="nav-button" onClick={() => navigate('/dashboard')}>Real Time Data</button>
       <button className="nav-button" onClick={() => navigate('/trade')}>Trade Execution</button>
       <button className="nav-button" onClick={() => navigate('/history')}>Trade History</button>
-      <button className="nav-button" onClick={() => navigate('/link-coinbase')}>Link Coinbase</button>
     </div>
   );
 }
